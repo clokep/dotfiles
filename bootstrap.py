a7486b64bd0c0f45037b3e33f68ce87a50825559
Patrick Cloke <clokep@patrick.cloke.us>
2026-08-18 14:45:58 -0400

    Rewrite bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python 
 
-# Basic Imports
 import os
 import sys
 
+from pathlib import Path
+
 # Global - add files you want to ignore in the current directory
-IGNORED_FILES = [".gitignore", "bootstrap.py", "README.md", "LICENSE"]
+IGNORED_FILES = [".gitignore", ".osx", "bootstrap.py", "README.md", "LICENSE", "Brewfile"]
 
 if __name__ == "__main__":
     """
@@ -13,28 +14,39 @@ if __name__ == "__main__":
     files into your home directory
     """
 
-    current_directory = os.getcwd()
-    home = os.getenv("HOME")
+    current_directory = Path(__file__).parent
+    home = Path(os.getenv("HOME"))
 
-    if not current_directory.endswith("dotfiles"):
+    if current_directory.name != "dotfiles":
         print("Are you running this outside of the dotfiles directory?")
         sys.exit(1)
 
     else:
-        dotfiles  = os.listdir(current_directory)
+        files  = sorted(current_directory.glob(".*"))
 
-        for f in dotfiles:
-            if f.endswith("~") or os.path.isdir(f) or (f in IGNORED_FILES):
-                print(f"Ignoring temp file or directory: {f}")
+        for f in files:
+            if f.name.endswith("~") or f.is_dir() or (f.name in IGNORED_FILES):
+                print(f"Ignoring: {f}")
                 continue
             
-            elif f.startswith("."):
-                original = current_directory + "/" + f
-                home_file = home + "/" + f
-                print(f"Linking: {original} --> {home_file}")
-                os.system(f"ln -si {original} {home_file}")
+            elif f.name.startswith("."):
+                home_file = home / f.name
+
+                # If file exists and points to the right spot, all good.
+                if home_file.resolve() == f:
+                    print(f"Ignoring: {f}")
+
+                # If file exists and doesn't point to the right spot, we need a human.
+                elif home_file.exists():
+                    print(f"Already exists: {f}")
+
+                # Otherwise, link the file.
+                else:
+                    print(f"Linking:  {home_file} --> {f}")
+                    home_file.symlink_to(f)
 
             else:
+                # This should never happen due to the glob.
                 print("We encountered some weird file and are ignoring it")
 
     print("Done linking...")
