c3ff5968678c6c019aec40e245dbd1675f008076
Patrick Cloke <clokep@patrick.cloke.us>
2026-08-28 16:58:13 -0400

    Support directories

--- bootstrap.py
+++ bootstrap.py
@@ -6,7 +6,43 @@ import sys
 from pathlib import Path
 
 # Global - add files you want to ignore in the current directory
-IGNORED_FILES = [".gitignore", ".osx", "bootstrap.py", "README.md", "LICENSE", "Brewfile"]
+IGNORED_FILES = [".git", ".gitignore", ".idea", ".osx", "bootstrap.py", "Brewfile", "LICENSE", "README.md"]
+
+def symlink_path(current_directory: Path, home: Path):
+    files = sorted(current_directory.iterdir())
+
+    for f in files:
+        if f.name.endswith("~") or (f.name in IGNORED_FILES):
+            print(f"Ignoring: {f}")
+            continue
+
+        if f.is_dir():
+            new_target = home / f.name
+            if not new_target.exists():
+                new_target.mkdir()
+            elif not new_target.is_dir():
+                print(f"Sub-directory path exists and is not a directory: {new_target}")
+                continue
+
+            symlink_path(f, home / f.name)
+            continue
+
+        else:
+            home_file = home / f.name
+
+            # If file exists and points to the right spot, all good.
+            if home_file.resolve() == f:
+                print(f"Already linked: {f}")
+
+            # If file exists and doesn't point to the right spot, we need a human.
+            elif home_file.exists():
+                print(f"Already exists: {f}")
+
+            # Otherwise, link the file.
+            else:
+                print(f"Linking:  {home_file} --> {f}")
+                home_file.symlink_to(f)
+
 
 if __name__ == "__main__":
     """
@@ -22,31 +58,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     else:
-        files  = sorted(current_directory.glob(".*"))
-
-        for f in files:
-            if f.name.endswith("~") or f.is_dir() or (f.name in IGNORED_FILES):
-                print(f"Ignoring: {f}")
-                continue
-            
-            elif f.name.startswith("."):
-                home_file = home / f.name
-
-                # If file exists and points to the right spot, all good.
-                if home_file.resolve() == f:
-                    print(f"Ignoring: {f}")
-
-                # If file exists and doesn't point to the right spot, we need a human.
-                elif home_file.exists():
-                    print(f"Already exists: {f}")
-
-                # Otherwise, link the file.
-                else:
-                    print(f"Linking:  {home_file} --> {f}")
-                    home_file.symlink_to(f)
-
-            else:
-                # This should never happen due to the glob.
-                print("We encountered some weird file and are ignoring it")
+        symlink_path(current_directory, home)
 
     print("Done linking...")
