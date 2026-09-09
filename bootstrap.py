c257b3a7a0fd4de450e2f96964e9b785ee304476
Patrick Cloke <clokep@patrick.cloke.us>
2026-09-09 11:33:54 -0400

    Fix-up ignoring files

--- bootstrap.py
+++ bootstrap.py
@@ -5,30 +5,30 @@ import sys
 
 from pathlib import Path
 
-# Global - add files you want to ignore in the current directory
-IGNORED_FILES = [".git", ".gitignore", ".idea", ".osx", "bootstrap.py", "Brewfile", "LICENSE", "README.md"]
+# Files to ignore in the root directory.
+IGNORED_FILES = {".git", ".gitignore", ".idea", ".osx", "bootstrap.py", "Brewfile", "LICENSE", "README.md"}
 
-def symlink_path(current_directory: Path, home: Path):
+def symlink_path(current_directory: Path, current_home: Path, ignored_files: set[str] = set()) -> None:
     files = sorted(current_directory.iterdir())
 
     for f in files:
-        if f.name.endswith("~") or (f.name in IGNORED_FILES):
+        if f.name.endswith("~") or (f.name in ignored_files):
             print(f"Ignoring: {f}")
             continue
 
         if f.is_dir():
-            new_target = home / f.name
+            new_target = current_home / f.name
             if not new_target.exists():
                 new_target.mkdir()
             elif not new_target.is_dir():
                 print(f"Sub-directory path exists and is not a directory: {new_target}")
                 continue
 
-            symlink_path(f, home / f.name)
+            symlink_path(f, current_home / f.name)
             continue
 
         else:
-            home_file = home / f.name
+            home_file = current_home / f.name
 
             # If file exists and points to the right spot, all good.
             if home_file.resolve() == f:
@@ -50,14 +50,13 @@ if __name__ == "__main__":
     files into your home directory
     """
 
-    current_directory = Path(__file__).parent
+    working_dir = Path(__file__).parent
     home = Path(os.getenv("HOME"))
 
-    if current_directory.name != "dotfiles":
+    if working_dir.name != "dotfiles":
         print("Are you running this outside of the dotfiles directory?")
         sys.exit(1)
 
-    else:
-        symlink_path(current_directory, home)
+    symlink_path(working_dir, home, IGNORED_FILES)
 
     print("Done linking...")
